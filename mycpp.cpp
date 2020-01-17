8c6df18e07261ed01592ef59203b6089a21fd2c8
Calvin-Varga <60004892+Calvin-Varga@users.noreply.github.com>
2020-01-17 09:05:48 -0500

    Add files via upload

---
+++ mycpp.cpp
@@ -0,0 +1,54 @@
+//Calvin Varga
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+ 	ifstream fin;
+ 	ofstream fout;
+	
+	// check if there are enough arguments
+	
+	if (argv[3] != NULL || argv[2] == NULL){
+		cout << "You need to enter two arguments!" << endl;
+		return 1;
+	}
+	
+	// open the first file
+	
+	fin.open(argv[1]);
+ 	
+	char c;
+
+ 	if (fin.fail()) // check if it is successful 
+ 	{
+ 		cerr << " Cannot open the input file!" << endl;
+ 		return 1;
+ 	}
+ 	
+
+	// open the second file
+	
+	fout.open(argv[2]);
+	
+ 	if (fout.fail())
+ 	{
+ 		cerr << " Cannot open the output file!" << endl;
+ 		return 1;
+ 	}
+ 	
+ 	while(fin.get(c)) 
+	{
+		fout << c;
+	}
+ 	
+ 	fin.close(); 
+
+ 	fout.close();
+
+ 	 return 0;
+
+ } 
